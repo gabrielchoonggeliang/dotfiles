b8dadf4537fd8eb51421daa549bcacc58f1bcbf0
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2023-01-30 14:47:28 +0800

    Modified: removed xcode-select --install

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ if [[ "$INSTALL" == "y" || "$INSTALL" == "N" ]]; then
     if [ "$INSTALL" = "N" ]; then
         echo "No changes made"
     else
-        xcode-select --install
         git clone https://github.com/gabrielchoonggeliang/dotfiles.git ~/.dotfiles
         ln -s ~/.dotfiles/.zshrc ~/.zshrc
         ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
